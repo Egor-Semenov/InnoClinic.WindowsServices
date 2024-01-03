1c8ba7f86ecb9b942b19beaf82e546183ffc58f9
Egor Semenov <egor123semenov@gmail.com>
2024-01-03 17:12:45 +0100

    Implement EmailNotification Windows Service

---
+++ BatFiles/EmailWinService_Create.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+InstallUtil.exe D:\InnoClinicWindowsServices\InnoClinic.WindowsServices\InnoClinic.WindowsServices\bin\Debug\InnoClinic.WindowsServices.exe
